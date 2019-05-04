2c4729308a3ab29816e68909a4a54eb77dbdc2be
Your Name <118735992@.con>
2019-05-04 19:44:44 +0800

    插件按需加载,准备第二次打包

--- src/lib/Plugins.js
+++ src/lib/Plugins.js
@@ -1,10 +1,39 @@
 // 导入vue
 import Vue from 'vue'
 // 导入饿了吗ui插件
-import ElementUI from 'element-ui';
+// import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
+// 饿了吗插件按需引入
+import {
+    Button,
+    Notification,
+    Carousel,
+    Switch,
+    Input,
+    InputNumber,
+    Pagination,
+    CarouselItem,
+    Form,
+    FormItem,
+    Radio,
+    Message,
+    MessageBox,} from 'element-ui';
+// 注册
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Switch);
+Vue.use(InputNumber);
+Vue.use(Pagination);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Radio);
+Vue.prototype.$message = Message;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$notify = Notification;
 
 // 导入iview插件
 import iView from 'iview';
